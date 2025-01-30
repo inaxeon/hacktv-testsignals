05430ca9b928f5ef53d7865b9f9639f2dafc835b
Matthew Millman <Matt@inaxeon.local>
2025-01-30 05:04:18 +0000

    Initial commit

---
+++ testsignal_sequence.sh
@@ -0,0 +1,26 @@
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal philips4x3 --text1 "HackTV" --text2 "Test" --clockmode off ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal philips4x3 --text1 "HackTV" --text2 "Test" --clockmode time ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal philips4x3 --text1 "HackTV" --text2 "Test" --clockmode datetime ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal fubk4x3 --text1 "HackTV" --text2 "Test" --clockmode off ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal fubk4x3 --text1 "HackTV" --text2 "Test" --clockmode time ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal fubk4x3 --text1 "HackTV" --text2 "Test" --clockmode datetime ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal philips16x9 --text1 "HackTV" --text2 "Test" --clockmode off ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal philips16x9 --text1 "HackTV" --text2 "Test" --clockmode time ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal philips16x9 --text1 "HackTV" --text2 "Test" --clockmode datetime ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal fubk16x9 --text1 "HackTV" --text2 "Test" --clockmode off ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal fubk16x9 --text1 "HackTV" --text2 "Test" --clockmode time ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal fubk16x9 --text1 "HackTV" --text2 "Test" --clockmode datetime ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal cbar ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal sinxx ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 13500000 --samplerate 13500000 --testsignal pulseandbar ./test_1khz_5sec.wav
+../src/hacktv -m pal --pixelrate 20000000 --samplerate 13500000 --testsignal philipsih ./test_1khz_5sec.wav
+../src/hacktv -m secam --pixelrate 13500000 --samplerate 13500000 --testsignal philips4x3 --text1 "HackTV" --text2 "Test" --clockmode off ./test_1khz_5sec.wav
+../src/hacktv -m secam --pixelrate 13500000 --samplerate 13500000 --testsignal philips4x3 --text1 "HackTV" --text2 "Test" --clockmode time ./test_1khz_5sec.wav
+../src/hacktv -m secam --pixelrate 13500000 --samplerate 13500000 --testsignal philips4x3 --text1 "HackTV" --text2 "Test" --clockmode datetime ./test_1khz_5sec.wav
+../src/hacktv -m ntsc --pixelrate 13500000 --samplerate 13500000 --testsignal philips4x3 --text1 "HackTV" --text2 "Test" --clockmode off ./test_1khz_5sec.wav
+../src/hacktv -m ntsc --pixelrate 13500000 --samplerate 13500000 --testsignal philips4x3 --text1 "HackTV" --text2 "Test" --clockmode time ./test_1khz_5sec.wav
+../src/hacktv -m ntsc --pixelrate 13500000 --samplerate 13500000 --testsignal philips4x3 --text1 "HackTV" --text2 "Test" --clockmode datetime ./test_1khz_5sec.wav
+../src/hacktv -m ntsc --pixelrate 13500000 --samplerate 13500000 --testsignal philips16x9 --text1 "HackTV" --text2 "Test" --clockmode off ./test_1khz_5sec.wav
+../src/hacktv -m ntsc --pixelrate 13500000 --samplerate 13500000 --testsignal philips16x9 --text1 "HackTV" --text2 "Test" --clockmode time ./test_1khz_5sec.wav
+../src/hacktv -m ntsc --pixelrate 13500000 --samplerate 13500000 --testsignal philips16x9 --text1 "HackTV" --text2 "Test" --clockmode datetime ./test_1khz_5sec.wav
+../src/hacktv -m ntsc --pixelrate 13500000 --samplerate 13500000 --testsignal cbar --text1 "HackTV" --text2 "Test" --clockmode off ./test_1khz_5sec.wav
